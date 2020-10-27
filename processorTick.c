f0a821c1bf344f9367d6a3b38c24c59226c27dd5
harshpanchalcs <cs19m026@smail.iitm.ac.in>
2020-10-27 13:01:18 +0530

    processTick

--- processorTick.c
+++ processorTick.c
@@ -7,14 +7,24 @@ void processorTick() __attribute__ (( naked ));
 void SIG_OUTPUT_COMPARE1A( void ) __attribute__ (( signal, naked));
 void processorTick()
 {
-	//portSAVE_CONTEXT();	
-	
+	/* This is a naked function so the context is saved. */
+    	
+	//portSAVE_CONTEXT();
+
+	/* Increment the tick count and check to see if the new tick value has caused a delay period to expire.  This function call can cause a task to become ready to run. */
+	//vTaskIncrementTick();
 	counter++;
 
+
+	/* See if a context switch is required. Switch to the context of a task made ready to run by vTaskIncrementTick() if it has a priority higher than the interrupted task. */
+
+	//vTaskSwitchContext();
+
+	/* Restore the context.  If a context switch has occurred this will restore the context of the task being resumed. */
+	
 	//portRESTORE_CONTEXT();
-	/*if(some process will unblock)
-	{
-	}*/
+
+	/* Return from this naked function. */
 	printf("processorTick\n");
 	asm volatile ( "ret" );
 }
